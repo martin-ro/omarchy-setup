a224979e8c9c27518faf45b30d33fa35042c0f9b
Martin Ro <martin-ro@users.noreply.github.com>
2026-09-10 21:21:05 +0800

    Install Herdr Next Agent plugin

--- install-herdr.sh
+++ install-herdr.sh
@@ -2,7 +2,15 @@
 
 set -e
 
+HERDR="$HOME/.local/bin/herdr"
+
 echo "Installing Herdr..."
 omarchy pkg drop herdr herdr-bin
 omarchy mise install herdr
-"$HOME/.local/bin/herdr" --version
+"$HERDR" --version
+
+if "$HERDR" plugin list | grep -q 'martinro.next-agent'; then
+  echo "Herdr Next Agent plugin already installed."
+else
+  "$HERDR" plugin install martin-ro/herdr-next-agent --yes
+fi
