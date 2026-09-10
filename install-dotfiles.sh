0f0e9fff8bf519fd9ab6ec3c8acb620c5984273f
Martin Ro <martin-ro@users.noreply.github.com>
2026-09-10 15:17:56 +0800

    Stow Hyprland configuration

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -8,7 +8,7 @@ if [ ! -d "$DOTFILES/.git" ]; then
   git clone https://github.com/martin-ro/dotfiles.git "$DOTFILES"
 fi
 
-mkdir -p "$HOME/.config/yazi"
-rm -f "$HOME/.config/yazi/yazi.toml"
+mkdir -p "$HOME/.config/yazi" "$HOME/.config/hypr"
+rm -f "$HOME/.config/yazi/yazi.toml" "$HOME/.config/hypr/looknfeel.lua"
 
-stow --restow --dir="$DOTFILES" --target="$HOME" yazi
+stow --restow --dir="$DOTFILES" --target="$HOME" yazi hyprland
